d3201f704fb63630f8f164db7f04b841696a0f39
littleNightK <dedrpkingdom2003@gmail.com>
2023-09-21 22:21:55 +0700

    Minor layout changes, added logout button

--- app/src/main/java/com/example/mycrypto/AfterLoginFragment.java
+++ app/src/main/java/com/example/mycrypto/AfterLoginFragment.java
@@ -1,17 +1,37 @@
 package com.example.mycrypto;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ImageButton;
+
 
 public class AfterLoginFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         ViewGroup root=(ViewGroup) inflater.inflate(R.layout.after_login_fragment,container,false);
+
+        ImageButton logoutbtn=root.findViewById(R.id.logoutbut);
+        logoutbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                MainActivity.isLogin = false;
+                Fragment fragment = new LoginFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frame_layout, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+
+        });
+
         return root;
     }
 
